3580a29b0286d5bb90a78113052c7e4a61dad01f
James Joyce <james_joyce@college.harvard.edu>
2020-12-06 16:01:55 -0500

    Delete DELETE.R

--- DELETE.R
+++
@@ -1,7 +0,0 @@
-combo <- read_csv("Final/ec_ter.csv")
-
-combo %>% 
-  ggplot(aes(x = econ_equal, y = attacks_per_mil)) +
-  geom_point() +
-  geom_smooth(method = MASS::glm.nb, color = "blue") +
-  theme_classic()
